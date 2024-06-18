d6717d118a5fb8348e732c20056e97e3c9d5f69c
sbandawar <sbandawar@dataeconomy.io>
2024-06-17 23:52:05 -0400

    resolved testing errors

--- src/app/services/album-service.service.spec.ts
+++ src/app/services/album-service.service.spec.ts
@@ -1,16 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AlbumService } from './album-service.service';
 
 describe('AlbumServiceService', () => {
   let service: AlbumService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AlbumService],
+    });
     service = TestBed.inject(AlbumService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a GET call to the API', () => {
+    const spy = spyOn(service, 'getAlbums').and.callThrough();
+    service.getAlbums();
+    expect(spy).toHaveBeenCalled();
+  });
 });
+
